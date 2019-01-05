1b9f784eb4fa2f69e99d8a4432482a898cb34e14
Thomas <thfoulon@student.42.fr>
2019-01-06 00:20:54 +0100

    method

--- get_next_line.c
+++ get_next_line.c
@@ -4,13 +4,20 @@ int	get_next_line(const int fd, char **s)
 {
 	int r = 0;
 	char buf[BUFF_SIZE + 1];
-	static char *file;
-	while ((r = read(fd, buf, BUFF_SIZE)) != 0)
+	static t_list *lst;
+	if (!s || fd < 1)  //cas erreur
+		return (-1);
+	ft_lstnew(); //creation lst
+	while ((r = read(fd, buf, BUFF_SIZE)) > 0)  // stockage fichier dans list
 	{
 		buf[r] = '\0';
-		ft_strappend(&file, buf);
+		ft_strappend((*lst)->content, buf);
 	}
-	return (0);
+	ft_strtok();  //decoupe du fichier
+	if ( '\n' )
+		return (1);	//termine fonction
+	if ( '\0' )  
+		return (0);
 }
 
 int	main(int ac, char **av)
@@ -20,9 +27,13 @@ int	main(int ac, char **av)
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
-		get_next_line(fd, &line);
+		if (fd == -1)
+		{
+			printf("open(); error\n");
+			return (1);
+		}
+		get_next_line(fd, &line)
+		printf("%s\n"', line);
 	}
-	else
-		printf("invalid arg\n");
 	return (0);
 }
