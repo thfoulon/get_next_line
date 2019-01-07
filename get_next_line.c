df2844f5656c09965d5c62f17090d29f4c5eed80
Thomas <thfoulon@student.42.fr>
2019-01-07 12:56:54 +0100

    algo

--- get_next_line.c
+++ get_next_line.c
@@ -8,16 +8,22 @@ int	get_next_line(const int fd, char **s)
 	if (!s || fd < 1)  //cas erreur
 		return (-1);
 	ft_lstnew(); //creation lst
-	while ((r = read(fd, buf, BUFF_SIZE)) > 0)  // stockage fichier dans list
+	if ((r = read(fd, buf, BUFF_SIZE)) > 0)  // stockage 1 read par fichier
 	{
 		buf[r] = '\0';
 		ft_strappend((*lst)->content, buf);
+	if (*lst)->fd = fd
+	while (!= '\n') //cherche d'une ligne dans le read
+		if ( '\n' || '\0')
+			ft_strtok();    //decoupe du fichier
+		if '\n'
+			return (1);	//termine fonction
+		if '\0'
+			return (0);
+		else
+		read again
 	}
-	ft_strtok();  //decoupe du fichier
-	if ( '\n' )
-		return (1);	//termine fonction
-	if ( '\0' )  
-		return (0);
+	return (-1);
 }
 
 int	main(int ac, char **av)
