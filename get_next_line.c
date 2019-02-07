f69706f0a1b03014f571f01efa23eb29c350d7ee
Thomas <thfoulon@student.42.fr>
2019-02-07 16:35:31 +0100

    route

--- get_next_line.c
+++ get_next_line.c
@@ -12,29 +12,29 @@
 
 #include "get_next_line.h"
 
-int		ft_next_line(char *s, char **line, int r, int fd)
+int		ft_next_line(char **s, char **line, int r, int fd)
 {
 	char	*tmp;
 	int		i;
 
 	i = 0;
-	while (s[i] != '\n' && s[i])
+	while (s[fd][i] != '\n' && s[fd][i] != '\0')
 		i++;
-	if (s[i] == '\n')
+	if (s[fd][i] == '\n')
 	{
-		*line = ft_strsub(s, 0, i);
-		tmp = ft_strdup(s + i + 1);
-		ft_strdel(&s);
-		s = ft_strdup(tmp);
-		if (s[0] == '\0')
-			ft_strdel(&s);
+		*line = ft_strsub(s[fd], 0, i);
+		tmp = ft_strdup(s[fd] + i + 1);
+		ft_strdel(&s[fd]);
+		s[fd] = tmp;
+		if (s[fd][0] == '\0')
+			ft_strdel(&s[fd]);
 	}
-	else if (s[i] == '\0')
+	else if (s[fd][i] == '\0')
 	{
 		if (r == BUFF_SIZE)
 			return (get_next_line(fd, line));
-		*line = ft_strdup(s);
-		ft_strdel(&s);
+		*line = ft_strdup(s[fd]);
+		ft_strdel(&s[fd]);
 		return (0);
 	}
 	return (1);
@@ -42,7 +42,7 @@ int		ft_next_line(char *s, char **line, int r, int fd)
 
 int		get_next_line(const int fd, char **line)
 {
-	static char	*s;
+	static char	*s[1024];
 	char		buff[BUFF_SIZE + 1];
 	char		*tmp;
 	int			r;
@@ -52,17 +52,17 @@ int		get_next_line(const int fd, char **line)
 	while ((r = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[r] = '\0';
-		if (!(s))
-			s = ft_strnew(1);
-		tmp = ft_strjoin(s, buff);
-		ft_strdel(&s);
-		s = tmp;
+		if (!(s[fd]))
+			s[fd] = ft_strnew(1);
+		tmp = ft_strjoin(s[fd], buff);
+		ft_strdel(&s[fd]);
+		s[fd] = tmp;
 		if (ft_strchr(buff, '\n'))
 			break ;
 	}
 	if (r < 0)
 		return (-1);
-	else if (r == 0 && (s == NULL || s[0] == '\0'))
+	else if (r == 0 && (s == NULL || s[fd][0] == '\0'))
 		return (0);
 	return (ft_next_line(s, line, r, fd));
 }
@@ -75,16 +75,12 @@ int		main(int ac, char **av)
 
 	if (ac == 2)
 	{
-		if ((fd = open(av[1], O_RDONLY)) < 0)
-		{
-			printf("open(); error\n");
-			return (1);
-		}
+		fd = open(av[1], O_RDONLY);
 		while ((r = get_next_line(fd, &s)) > 0)
 			printf("%d, %s\n", r, s);
 		printf("%d, %s\n", r, s);
 	}
 	else
-		printf("arg error\n");
+		printf("open(); error\n");
 	return (0);
 }
